115b7752d0119465fc92dfa9fcf4fb022e4fe5f1
MARNUSH KHAN K <marnushkhan.k@cognizant.com>
2017-03-06 10:18:57 +0530

    Initial Commit

---
+++ KXThumbCircularProgressBar/KXThumbCircularProgressBar.h
@@ -0,0 +1,19 @@
+//
+//  KXThumbCircularProgressBar.h
+//  KXThumbCircularProgressBar
+//
+//  Created by khan on 06/03/17.
+//  Copyright © 2017 Appyte. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KXThumbCircularProgressBar.
+FOUNDATION_EXPORT double KXThumbCircularProgressBarVersionNumber;
+
+//! Project version string for KXThumbCircularProgressBar.
+FOUNDATION_EXPORT const unsigned char KXThumbCircularProgressBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KXThumbCircularProgressBar/PublicHeader.h>
+
+
