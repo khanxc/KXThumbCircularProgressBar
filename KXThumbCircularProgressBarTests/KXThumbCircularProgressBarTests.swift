04f03e536262e86e50f9684d35bef0b15bcc7c38
MARNUSH KHAN K <marnushkhan.k@cognizant.com>
2017-03-06 12:55:12 +0530

    added test cases

--- KXThumbCircularProgressBarTests/KXThumbCircularProgressBarTests.swift
+++ KXThumbCircularProgressBarTests/KXThumbCircularProgressBarTests.swift
@@ -16,6 +16,14 @@ class KXThumbCircularProgressBarTests: XCTestCase {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
+    
+    func initiateCircularProgressbar() {
+        
+    }
+    
+    
+    
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
